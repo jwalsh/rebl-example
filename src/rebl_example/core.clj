5f3b73b65f0c58dcec8e4e6f84e1623d6897c4e9
Jason Walsh <jwalsh@saylent.com>
2018-11-29 21:09:24 -0500

    Example data structures

--- src/rebl_example/core.clj
+++ src/rebl_example/core.clj
@@ -1,6 +1,19 @@
 (ns rebl-example.core)
 
-(defn foo
+(def a 1)
+
+(def b "b")
+
+(def c (range 30))
+
+(def b (/ 1 0))
+
+(def c {:a 1
+        :b 2})
+
+(def d [1 2 3 4 5])
+
+(defn f
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
