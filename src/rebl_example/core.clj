f69d72975de60062caf1cb76bc3cb0536bf545d3
Jason Walsh <jwalsh@saylent.com>
2018-11-30 11:35:27 -0500

    Records and pagination

--- src/rebl_example/core.clj
+++ src/rebl_example/core.clj
@@ -6,6 +6,11 @@
 
 (def c (range 30))
 
+(quote
+ c
+
+ )
+
 (def b (/ 1 0))
 
 (def c {:a 1
@@ -13,7 +18,33 @@
 
 (def d [1 2 3 4 5])
 
+(def e (reduce + d))
+(reduce + d)
+
+(quote
+ e
+
+ )
+
 (defn f
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
+
+(def s
+  (slurp "https://www.gutenberg.org/files/3207/3207.txt"))
+
+(reduce #(assoc %1 %2 (inc (%1 %2 0)))
+        {}
+        (re-seq #"\w+" s))
+
+(for [suit [:♠ :♥ :♦ :♣]
+      rank (range 1 14)]
+  {:suit suit :rank rank})
+
+
+(defrecord Transaction [id timestamp account])
+(defrecord Account [customer])
+(defrecord Card [customer])
+(defrecord Customer [fname lname address email])
+(defrecord Address [street city state zip])
